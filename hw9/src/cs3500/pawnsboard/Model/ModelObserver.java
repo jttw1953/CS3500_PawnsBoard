16c6793087582ef74229cafd3647c837d89fe4cf
dkarma68 <148018958+dkarma68@users.noreply.github.com>
2025-04-18 00:37:36 +0200

    Add files via upload

---
+++ hw9/src/cs3500/pawnsboard/Model/ModelObserver.java
@@ -0,0 +1,21 @@
+package cs3500.pawnsboard.Model;
+
+/**
+ * Represents an observer that wants to be notified of important
+ * changes in the PawnsBoardModel:
+ *  - when the current player changes,
+ *  - when the game ends, etc.
+ */
+public interface ModelObserver {
+
+    /**
+     * Called whenever the active player changes.
+     */
+    void onTurnChanged(PlayerColor newActivePlayer);
+
+    /**
+     * Called when the game ends.
+     * @param winner the winning color, or null if tie
+     */
+    void onGameOver(PlayerColor winner);
+}
